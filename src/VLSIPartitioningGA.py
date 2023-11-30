ffa0e64f83dbfa2b625328e28f925261a6a9bee7
echinoislas <echinoislas+github@gmail.com>
2023-11-30 15:53:16 -0800

    updated VSLI with correct circuit

--- src/VLSIPartitioningGA.py
+++ src/VLSIPartitioningGA.py
@@ -9,26 +9,46 @@ class VLSIPartitionGA:
         self.n_partitions = n_partitions
         self.pop_size = pop_size
 
-        self.connectivity_matrix = self.create_connectivity_matrix()
-        self.net_matrix = self.create_net_matrix()
+        
+        self.connectivity_matrix = [
+            [0, 0, 0, 0, 1, 0, 0, 0], # 0
+            [0, 0, 0, 0, 0, 1, 0, 0], # 1
+            [0, 0, 0, 1, 0, 1, 0, 0], # 2
+            [0, 0, 1, 0, 1, 0, 0, 0], # 3
+            [1, 0, 0, 1, 0, 0, 1, 0], # 4
+            [0, 1, 1, 0, 0, 0, 1, 0], # 5
+            [0, 0, 0, 0, 1, 1, 0, 1], # 6
+            [0, 0, 0, 0, 0, 0, 1, 0]  # 7
+        ]
+        self.net_matrix = [
+            [1, 0, 0, 0, 1, 0, 0, 0], # 0
+            [0, 1, 0, 0, 0, 1, 0, 0], # 1
+            [0, 0, 1, 1, 0, 0, 0, 0], # 2
+            [0, 0, 1, 0, 0, 1, 0, 0], # 3
+            [0, 0, 0, 1, 1, 0, 0, 0], # 4
+            [0, 0, 0, 0, 1, 0, 1, 0], # 5
+            [0, 0, 0, 0, 0, 1, 1, 0], # 6
+            [0, 0, 0, 0, 0, 0, 1, 1] # 7 
+            
+        ]
         self.toolbox = base.Toolbox()
         self.setup_deap()
 
-    def create_connectivity_matrix(self):
-        matrix = np.zeros((len(self.signals), len(self.signals)), dtype=int)
-        for src, dests in self.connections.items():
-            for dest in dests:
-                matrix[self.signals.index(src), self.signals.index(dest)] = 1
-        return matrix
+    # def create_connectivity_matrix(self):
+    #     matrix = np.zeros((len(self.signals), len(self.signals)), dtype=int)
+    #     for src, dests in self.connections.items():
+    #         for dest in dests:
+    #             matrix[self.signals.index(src), self.signals.index(dest)] = 1
+    #     return matrix
 
-    def create_net_matrix(self):
-        matrix = np.zeros((len(self.signals), len(self.signals)), dtype=int)
-        for i, signal in enumerate(self.signals):
-            if signal in self.connections:
-                for dest in self.connections[signal]:
-                    matrix[i, self.signals.index(dest)] = 1
-                    matrix[self.signals.index(dest), i] = 1
-        return matrix
+    # def create_net_matrix(self):
+    #     matrix = np.zeros((len(self.signals), len(self.signals)), dtype=int)
+    #     for i, signal in enumerate(self.signals):
+    #         if signal in self.connections:
+    #             for dest in self.connections[signal]:
+    #                 matrix[i, self.signals.index(dest)] = 1
+    #                 matrix[self.signals.index(dest), i] = 1
+    #     return matrix
 
     def gen_chromosome(self, net_mat):
         modules = list(range(len(net_mat)))
@@ -37,13 +57,23 @@ class VLSIPartitionGA:
         for i, module in enumerate(modules):
             partitions[i % self.n_partitions].append(module)
 
-        chromosome = np.zeros((len(self.signals)), dtype=int)
+        chromosome = np.zeros((len(self.connectivity_matrix)), dtype=int)
         
         for p, partition in enumerate(partitions):
             for module in partition:
                 chromosome[module] = p
 
         return chromosome
+    
+    def eval_fitness(self, individual):
+        # Calculate the number of nets crossing partitions
+        nets_crossing = 0
+        for i, row in enumerate(self.net_matrix):
+            for j, col in enumerate(row):
+                if col == 1 and individual[i] != individual[j]:
+                    nets_crossing += 1
+        return (nets_crossing,)
+    
     
     def setup_deap(self):
         creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
@@ -52,6 +82,16 @@ class VLSIPartitionGA:
         self.toolbox.register("chromosome", self.gen_chromosome, net_mat=self.net_matrix)
         self.toolbox.register("individual", tools.initIterate, creator.Individual, self.toolbox.chromosome)
         self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
+
+        # Tournament Selection with a tournament size of 3
+        self.toolbox.register("select", tools.selTournament, tournsize=3)
+        # One-point Crossover
+        self.toolbox.register("mate", tools.cxOnePoint)
+        # Mutation: Randomly change the partition assignment of a module
+        self.toolbox.register("mutate", tools.mutUniformInt, low=1, up=self.n_partitions, indpb=0.1)
+        # Fitness function
+        self.toolbox.register("evaluate", self.eval_fitness)
+
 
     def create_population(self):
         return self.toolbox.population(n=self.pop_size)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 import numpy as np
 from VLSIPartitioningGA import VLSIPartitionGA
+import random
+import deap.tools as tools
+
+# Global parameters for the genetic algorithm
+CROSSOVER_PROB = 0.7  # Probability with which two individuals are crossed
+MUTATION_PROB = 0.2   # Probability of mutating an individual
+
 
 if __name__ == "__main__":
     # Usage
@@ -19,6 +26,45 @@ if __name__ == "__main__":
     population = ga.create_population()
     ga.display_matrices()
 
-    print("\nPopulation:")
+    # Assuming you have a method in your class to evaluate fitness
     for individual in population:
-        print(individual)
+        individual.fitness.values = ga.eval_fitness(individual)
+
+    # Number of generations
+    n_generations = 50
+
+    for gen in range(n_generations):
+        # Select the next generation individuals
+        offspring = ga.toolbox.select(population, len(population))
+
+        # Clone the selected individuals
+        offspring = list(map(ga.toolbox.clone, offspring))
+
+        # Apply crossover and mutation on the offspring
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+            if random.random() < CROSSOVER_PROB:
+                ga.toolbox.mate(child1, child2)
+                del child1.fitness.values
+                del child2.fitness.values
+
+        for mutant in offspring:
+            if random.random() < MUTATION_PROB:
+                ga.toolbox.mutate(mutant)
+                del mutant.fitness.values
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = map(ga.toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        # Replace the old population by the offspring
+        population[:] = offspring
+
+        # Optional: Print out statistics or log data here
+
+    # Extracting the best solution
+    best_ind = tools.selBest(population, 1)[0]
+    print(f"Best Individual: {best_ind}")
+    print(f"Best Fitness: {best_ind.fitness.values}")
+
