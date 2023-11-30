3f9a7ae0552f5a5fab63afe97d875356beb8b07e
echinoislas <echinoislas+github@gmail.com>
2023-11-30 14:30:55 -0800

    updated main

--- src/VLSIPartitioningGA.py
+++ src/VLSIPartitioningGA.py
@@ -52,25 +52,3 @@ class VLSIPartitionGA:
     def display_matrices(self):
         print("Connectivity Matrix:\n", self.connectivity_matrix)
         print("\nNet Matrix:\n", self.net_matrix)
-
-if __name__ == "__main__":
-    # Usage
-    signals = ["a", "b", "sel", "not_sel", "a_and_not_sel", "b_and_sel", "out"]
-    connections = {
-        "sel": ["not_sel", "b_and_sel"],
-        "not_sel": ["a_and_not_sel"],
-        "a": ["a_and_not_sel"],
-        "b": ["b_and_sel"],
-        "a_and_not_sel": ["out"],
-        "b_and_sel": ["out"]
-    }
-    n_partitions = 3
-    pop_size = 13
-
-    ga = VLSIPartitionGA(signals, connections, n_partitions, pop_size)
-    population = ga.create_population()
-    ga.display_matrices()
-
-    print("\nPopulation:")
-    for individual in population:
-        print(individual)
--- src/main.py
+++ src/main.py
@@ -1,17 +1,9 @@
 import numpy as np
-
-from genetic_test import GeneticAlgorithm
-import deap.tools as tools
+from VLSIPartitioningGA import VLSIPartitionGA
 
 if __name__ == "__main__":
-    # Define the signals
+    # Usage
     signals = ["a", "b", "sel", "not_sel", "a_and_not_sel", "b_and_sel", "out"]
-
-    # Initialize matrices
-    connectivity_matrix = np.zeros((len(signals), len(signals)), dtype=int)
-    net_matrix = np.zeros((len(signals), len(signals)), dtype=int)
-
-    # Define connections for the connectivity matrix
     connections = {
         "sel": ["not_sel", "b_and_sel"],
         "not_sel": ["a_and_not_sel"],
@@ -20,34 +12,13 @@ if __name__ == "__main__":
         "a_and_not_sel": ["out"],
         "b_and_sel": ["out"]
     }
+    n_partitions = 3
+    pop_size = 13
 
-    # Populate the connectivity matrix
-    for src, dests in connections.items():
-        for dest in dests:
-            connectivity_matrix[signals.index(src), signals.index(dest)] = 1
-
-    # Populate the net matrix
-    for i, signal in enumerate(signals):
-        if signal in connections:
-            for dest in connections[signal]:
-                net_matrix[i, signals.index(dest)] = 1
-                net_matrix[signals.index(dest), i] = 1
-
-    # Display the matrices
-    print("Connectivity Matrix:\n", connectivity_matrix)
-    print("\nNet Matrix:\n", net_matrix)
-
-    # Example usage
-    # fitness_evaluator = MyFitnessEvaluator()
-    crossover_operator = tools.cxTwoPoint
-    mutation_operator = tools.mutFlipBit
-    selection_operator = tools.selTournament
-
-    ga = GeneticAlgorithm(None, crossover_operator, mutation_operator, selection_operator)
-    pop = ga.create_population(connectivity_matrix, net_matrix, 3, 10)
+    ga = VLSIPartitionGA(signals, connections, n_partitions, pop_size)
+    population = ga.create_population()
+    ga.display_matrices()
 
     print("\nPopulation:")
-    for individual in pop:
+    for individual in population:
         print(individual)
-    
-    
